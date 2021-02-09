ee9db931110af8f80da8003fe7355d27d3df5365
Joel David C <jdcruzzamora@gmail.com>
2021-02-08 18:41:34 -0600

    Volvi a poner lo necesario xD

--- Tablas.sql
+++ Tablas.sql
@@ -98,3 +98,75 @@ CREATE TABLE Conductor_Camion (
 	FOREIGN KEY (Placa) REFERENCES Camion(Placa)
 		ON DELETE CASCADE ON UPDATE CASCADE
 )
+
+
+
+--Llenado de datos ----------------------------------------------
+
+--Llenar las rutas. Deben ser 4
+insert into Rutas values(1,'Ruta 1')
+insert into Rutas values(2,'Ruta 2')
+insert into Rutas values(3,'Ruta 3')
+insert into Rutas values(4,'Ruta 4')
+
+--Llenar los C_locales
+insert into C_Local(CodigoC_Local,Nombre)
+values ('CODIGOLOCAL1','Manuel Sanchez')
+insert into C_Local(CodigoC_Local,Nombre)
+values ('CODIGOLOCAL2','Jose Romero')
+insert into C_Local(CodigoC_Local,Nombre)
+values ('CODIGOLOCAL3','Francisco Rodriguez')
+select * from Rutas
+
+--Paquetes internacionales -------------------------------------
+
+--Paquete 1
+insert into Paquete
+values('XZ_1234','Direccion1',2,'Miguel Sanchez')
+insert into Internacional(Codigo,CodigoC_Local,FEntrega)
+values('XZ_1234','CODIGOLOCAL1','2020-02-22')
+
+--Paquete 2
+insert into Paquete
+values('ZA_1234','Direccion2',1.3,'Jose Romero')
+insert into Internacional(Codigo,CodigoC_Local,FEntrega,LineaAerea)
+values('ZA_1234','CODIGOLOCAL2','2020-03-22','American Airlines')
+
+--Paquetes nacionales
+
+--Paquete 3
+insert into Paquete
+values('KF_1234','Direccion3',1.8,'Pedro Sosa')
+insert into Conductor(RFC,Nombre,Direccion,IdRuta)
+values('MELM8305281H0','Juan Diaz', 'Direccion de Juan',1)
+insert into Nacional(Codigo,CiudadDestino,IdRuta,RFC)
+values('KF_1234','Monterrey',1,'MELM8305281H0')
+insert into Camion(Placa,CargaMaxima,CiudadResguardo)
+values('YBU-80-66',3800,'CDMX')
+insert into Conductor_Camion(RFC,Placa,Fecha)
+values('MELM8305281H0','YBU-80-66','2020-12-10')
+
+--Paquete 4
+insert into Paquete
+values('KE_1334','Direccion4',0.9,'Adrian Perez')
+insert into Conductor(RFC,Nombre,Direccion,IdRuta)
+values('LUNM8305281H0','Adrian Perez', 'Direccion de Adrian',3)
+insert into Nacional(Codigo,CiudadDestino,IdRuta,RFC)
+values('KE_1334','Cordoba',3,'LUNM8305281H0')
+insert into Camion(Placa,CargaMaxima,CiudadResguardo)
+values('AXA-80-66',3800,'Guadalajara')
+insert into Conductor_Camion(RFC,Placa,Fecha)
+values('LUNM8305281H0','AXA-80-66','2020-12-15')
+
+--Paquete 5
+insert into Paquete
+values('DF_1226','Direccion5',2,'Daniel Ramos')
+insert into Conductor(RFC,Nombre,Direccion,IdRuta)
+values('AKGM8305281H0','Daniel Ramos', 'Direccion de Daniel',2)
+insert into Nacional(Codigo,CiudadDestino,IdRuta,RFC)
+values('DF_1226','CDMX',2,'AKGM8305281H0')
+insert into Camion(Placa,CargaMaxima,CiudadResguardo)
+values('YAU-80-66',4000,'Tijuana')
+insert into Conductor_Camion(RFC,Placa,Fecha)
+values('AKGM8305281H0','YAU-80-66','2020-12-2') 
+) 
