2ea55dcb25a27231cabddca57b2a4c9a48d90bbb
israel mejia <ihmaisrael@gmail.com>
2021-02-09 21:51:30 -0600

    Commit para unir lo que lleva joel del trigger que faltaba y correcciones de mi procedimiento almacenado

--- Procedimientos.sql
+++ Procedimientos.sql
@@ -21,7 +21,8 @@ Este procedimiento deberá mostrar lo siguiente ;
 
 go --	Creacion del procedimiento Almacenado
 --create
-alter procedure SP_Datos_Paquete_Envio @codigo varchar(7)
+alter 
+procedure SP_Datos_Paquete_Envio @codigo varchar(7)
 as
 	if ( @codigo in (select  N.Codigo --Si es nacional
 					FROM Paquete as P
@@ -37,8 +38,8 @@ as
 						on ( P.Codigo = N.Codigo )
 					inner join Conductor as C --Trae los datos de su conductor
 						on (C.RFC = N.RFC)
-					inner join Rutas as R -- Para mostrar la ruta
-						on ( R.IdRuta = N.IdRuta )
+					inner join Rutas_conductor as R -- Para mostrar la ruta
+						on ( R.RFC = C.RFC )
 					inner join Conductor_Camion as CC
 						on (C.RFC = CC.RFC)
 			where N.Codigo = @codigo
@@ -63,14 +64,16 @@ as
 		print'Codigo ' + @codigo + ' no es valido';
 
 
-
+GO
 --Probando el procedimiento
-EXEC SP_Datos_Paquete_Envio 'ZA_1234'
---'DF_1226' --Nacional --Repetido
---'FF_1234' --Nacional 
---'ZA_1234' --Internacional
+EXEC SP_Datos_Paquete_Envio 'ZA_1234'-- Internacional
+EXEC SP_Datos_Paquete_Envio 'DF_1226' --Nacional
+--'DF_1226' --Nacional  
+--'KF_1234' --Nacional 
+--'ZA_1234' --Internacional 
 
 --where N.CiudadDestino is not null ) )
+
  
 GO
 
@@ -85,33 +88,11 @@ FROM Paquete as P
 	 FULL JOIN Internacional as I
 		on (I.Codigo = P.Codigo)--Hacemos un full join para traer los que son Internacionales y si no, esos datos los marque null
 
-
+		
 --where N.CiudadDestino is not null --Para revisar los envios que sean unicamente nacionales
 
 where I.Codigo is not null --Para revisar los envios que sean unicamente Internacionales
 
 
 
- /*6.Crear un SP que muestre todo sobre un producto que 
-busque un cliente
-Nota: el cliente puede ingresar parte del nombre del producto, 
-en otras palabra mostrar todo lo que coincida con lo ingresado
-por el cliente.
-EXEC SP_ListarProducto(NombreProducto)*/
-go
-CREATE PROCEDURE SP_ListarProducto @NombreProducto VARCHAR(20)
-AS
- SELECT * FROM Products P
- WHERE P.ProductName LIKE CONCAT('%',@NombreProducto,'%')
-
-
   
-
- EXEC SP_ListarProducto 'queso'
- 
- SELECT * FROM Products
-
-
- delete  
- from Paquete 
- where Codigo ='DF_1226' --'KF_1234'
