f0a3d63da274944a0141ad48fbf8df3e6c108a3d
israel mejia <ihmaisrael@gmail.com>
2021-02-10 13:11:40 -0600

    Modifico mi trigger para que actualice al ultimo numero de envios realizados por ciudad

--- Triggers.sql
+++ Triggers.sql
@@ -84,24 +84,32 @@ y la ciudad de destino y además muestre cuantos envíos se han hecho a dicha ci
 destino(en otras palabras que guarde en un campo en número de envíos )
 Se deberá mostrar el correcto funcionamiento de los disparadores.
 */
-
+ 
 go
-create trigger T_EnviosNacionales
+create  trigger T_EnviosNacionales
 on Nacional 
 for insert
 as
 	begin
-	declare @tCodigo varchar(7) = (select Codigo from inserted)
-	declare @tCiudadDestino varchar(50) = (select CiudadDestino from inserted)
-	declare @tEnviosTotales int = (select count(*) from inserted group by CiudadDestino)
+	declare @tCodigo varchar(7) = (select Codigo from inserted)								-- Se toma el codigo del paquete nacional que vamos a ocupar
+	declare @tCiudadDestino varchar(50) = (select CiudadDestino from inserted)				--Se toma la ciudad del paquete insertado
+	declare @tEnviosTotales int = (select count(*) from Nacional
+									where CiudadDestino = @tCiudadDestino )			--Se cuentan cuantos envios se han registrado en esa ciudad y se lo asignamos a @tEnviosTotales
 	
+	--insertamos los valores obtenidos en el trigger a la tabla Envios nacionales
 	insert into  EnviosNacionales( IdEnvio, CiudadDestino, EnviosTotalesCiudad)
 		values (@tCodigo , @tciudadDestino, @tEnviosTotales )
+		
+	
+	--Actualizamos los envios totales que hay en esa ciudad en todos los registros
+	update  EnviosNacionales 
+	set EnviosTotalesCiudad = @tEnviosTotales
+	WHERE CiudadDestino =  @tCiudadDestino
 
 	print('Se registro el Envio Nacional')
 	end
 
-
+select * from Nacional
 
 
 --Probando el trigger
@@ -114,6 +122,40 @@ values('TR_1234','Monterrey','MELM8305281H0')
 
 insert into Rutas_nacional(Codigo, Ruta)
 values('TR_1234', 'Ruta 1')
+
+insert into Conductor_Camion(RFC,Placa,Fecha)
+values('MELM8305281H0','YBU-80-66','2020-12-10')
+
+insert into Rutas_conductor(RFC, Ruta)
+values('MELM8305281H0', 'Ruta 1')
+
+--Probando el trigger
+--Paquete 7 --Para corroborar el funcionamiento del trigger
+insert into Paquete(Codigo, Direccion, Peso, Destinatario)
+values('TR_2234','Direccion3',1.8,'Pedro Sosa')
+
+insert into Nacional(Codigo,CiudadDestino,RFC)
+values('TR_2234','Monterrey','MELM8305281H0')
+
+insert into Rutas_nacional(Codigo, Ruta)
+values('TR_2234', 'Ruta 1')
+
+insert into Conductor_Camion(RFC,Placa,Fecha)
+values('MELM8305281H0','YBU-80-66','2020-12-10')
+
+insert into Rutas_conductor(RFC, Ruta)
+values('MELM8305281H0', 'Ruta 1')
+
+--Probando el trigger
+--Paquete 8 --Para corroborar el funcionamiento del trigger
+insert into Paquete(Codigo, Direccion, Peso, Destinatario)
+values('TR_2238','Direccion3',1.8,'Pedro Sosa')
+
+insert into Nacional(Codigo,CiudadDestino,RFC)
+values('TR_2238','Monterrey','MELM8305281H0')
+
+insert into Rutas_nacional(Codigo, Ruta)
+values('TR_2238', 'Ruta 1')
 
 insert into Conductor_Camion(RFC,Placa,Fecha)
 values('MELM8305281H0','YBU-80-66','2020-12-10')
